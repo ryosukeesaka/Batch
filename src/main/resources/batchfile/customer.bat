f6fdb8928b900bfe13be559de338306dcc847cbe
ryosuke.esaka.yu <ryosuke.esaka.yu@ark-sys.co.jp>
2022-01-07 17:33:10 +0900

    受注バッチの追加

---
+++ src/main/resources/batchfile/customer.bat
@@ -0,0 +1,6 @@
+@echo off
+
+cd C:\Users\ryosuke.esaka.yu\Desktop\SpringBatchSample\demo\target
+
+Java -Dspring.batch.job.names=mCustomerJob -jar demo.jar
+
---
+++ src/main/resources/batchfile/order_orderDetail.bat
@@ -0,0 +1,5 @@
+@echo off
+
+cd C:\Users\ryosuke.esaka.yu\Desktop\SpringBatchSample\demo\target
+
+Java -Dspring.batch.job.names=tOrderJob -jar demo.jar
